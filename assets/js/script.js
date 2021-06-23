503cf85379010fb89176624afed7c70aeb81c66b
MiaTothova <21tothova@gmail.com>
2021-06-23 16:51:19 +0000

    Redo js code. Add start and generate shuffled questions functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,34 @@
+// targets ID in html
 const startButton = document.getElementById('start-btn')
-const questionContainer = document.getElementById('question-container')
-const questionElement = document.getElementById('question')
-const answerButtons = document.getElementById('answer-buttons')
-
-let shuffledQuestions, currentQuestion
-
 startButton.addEventListener('click', startGame)
 
+const questtionContainer = document.getElementById('question-container')
+let shuffledQuestions, currentQuestionIndex
+
 function startGame() {
-    console.log('started')
+    //hide start btn when clicked
     startButton.classList.add('hide')
-    questionContainer.classList.remove('hide')
+    //removes hide class from container
+    questtionContainer.classList.remove('hide')
+    //generates random questions
     shuffledQuestions = questions.sort(() => Math.random() - .5)
-    currentQuestion = 0
+    currentQuestionIndex = 0
     setNextQuestion()
-}
+} 
+
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
 
 function setNextQuestion() {
-    showQuestion(shuffledQuestions[currentQuestion])
+    showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
 function showQuestion(question) {
     questionElement.innerText = question.question
+
 }
 
+// 4. list of questions
 const questions = [
     {
         question: 'How many horns did a triceratops have?',
@@ -33,5 +38,97 @@ const questions = [
             {text: '1', correct: false}
 
         ] 
+    },
+    {
+        question: 'What does the word dinosaur mean? ',
+        answers: [
+            { text: 'Big lizard', correct: false},
+            { text: 'Hungry lizard', correct: false},
+            {text: 'Terrible lizard', correct: true}
+
+        ] 
+    },
+    {
+        question: 'Dinosaurs first appeared in which geological period?',
+        answers: [
+            { text: 'Triassic', correct: true},
+            { text: 'Jurassic', correct: false},
+            {text: 'Cretaceous ', correct: false}
+
+        ] 
+    },
+    {
+        question: 'What is the study of fossils called? ?',
+        answers: [
+            { text: 'Ice-creamology ', correct: false},
+            { text: ' Palaeontology', correct: true},
+            {text: 'Historiology ', correct: false}
+
+        ] 
+    },
+    {
+        question: 'When did dinosaurs go extinct?',
+        answers: [
+            { text: 'Around 85 million years ago ', correct: false},
+            { text: 'Around 75 million years ago ', correct: false},
+            {text: 'Around 65 million years ago', correct: true}
+
+        ] 
+    },
+    {
+        question: 'How many horns did a triceratops have?',
+        answers: [
+            { text: '4', correct: false},
+            { text: '3', correct: true},
+            {text: '1', correct: false}
+
+        ] 
+    },
+    {
+        question: 'Why did dinosaurs go extinct?',
+        answers: [
+            { text: 'They froze during Ice-age ', correct: false},
+            { text: 'They were killed by an asteroid', correct: true},
+            {text: 'They magically disappeared ', correct: false}
+
+        ] 
+    },
+    {
+        question: 'What was the first dinosaur to be discovered?',
+        answers: [
+            { text: 'Mosasaurus ', correct: false},
+            { text: 'T-rex ', correct: false},
+            {text: 'Megalosaurus', correct: true}
+
+        ] 
+    },
+    {
+        question: 'How many claws did a velociraptor have on each hand?',
+        answers: [
+            { text: '6', correct: false},
+            { text: '3', correct: true},
+            {text: '5', correct: false}
+
+        ] 
+    },
+    {
+        question: 'Which dinosaur was the size of a chicken?',
+        answers: [
+            { text: ' Dilong', correct: false},
+            { text: 'Compsognathus', correct: true},
+            {text: 'Nemicolopterus ', correct: false}
+
+        ] 
+    },
+    {
+        question: 'True or false: birds evolved from dinosaurs ',
+        answers: [
+            { text: 'True', correct: true},
+            { text: 'False', correct: false},
+            // add hide class to answer button here????
+           
+        ] 
     }
 ]
+
+
