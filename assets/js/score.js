eb450c1e3a3bf8c038c558efbb4475a02509c11b
MiaTothova <21tothova@gmail.com>
2021-06-27 22:04:29 +0000

    Add basic structure

---
+++ assets/js/score.js
@@ -0,0 +1,6 @@
+const homeButton = document.getElementById('home-btn') //today
+
+document.querySelector('#home-btn').addEventListener('click', () => { /////doesnt work why????
+    window.location.href = 'index.html';
+})
+
