3ce0a6bee356bab63222636961ed7be80fbd77f0
MiaTothova <21tothova@gmail.com>
2021-06-28 13:23:05 +0000

    Add const variable to potential functions

--- assets/js/score.js
+++ assets/js/score.js
@@ -2,5 +2,14 @@ const homeButton = document.getElementById('home-btn') //today
 
 document.querySelector('#home-btn').addEventListener('click', () => { /////doesnt work why????
     window.location.href = 'index.html';
+    console.log('I was clicked')
 })
+
+
+const username = document.getElementById('#username');
+const  saveScoreButton = document.getElementById('#score-board-btn');
+const  finalScore = document.getElementById('#final-score');
+
+
+
 
